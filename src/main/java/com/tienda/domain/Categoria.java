3bc731d0f3d73d7987c37d876e4bd4357cc28307
laicy <laicy@Laicy-05112004>
2023-06-26 16:29:54 -0600

    semana 6 sin errores

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,64 @@
+
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="categoria")
+public class Categoria implements Serializable {
+   private static  final long serialVersionUID = 1L;
+ 
+  @Id   
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name="id_categoria")
+  private long  idCategoria;
+  private String descripcion;
+  private String rutaImagen;
+  private boolean activo;  
+
+    public long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+  
+  
+}
